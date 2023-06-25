0eb25ac94465bd242996f391a7835342cf7d7f9d
sillyas2010 <sillyas2010@gmail.com>
2023-06-25 22:11:35 +0300

    generate favicon, change font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -12,6 +14,10 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+				sans: ["var(--font-poppins)", ...defaultTheme.fontFamily.sans],
+				display: ["var(--font-poppins)"],
+			},
     },
   },
   plugins: [],
