55ac0f7d6b25173d465d0d510640891948eb106d
sillyas2010 <sillyas2010@gmail.com>
2023-09-20 22:11:07 +0300

    fix typing for twin props

--- types/twin.d.ts
+++ types/twin.d.ts
@@ -2,6 +2,13 @@
 import 'twin.macro'
 import { ComponentPropsWithRef, ComponentType } from 'react'
 import styledImport, { CSSProp, css as cssImport } from 'styled-components'
+import { TwStyle } from 'twin.macro'
+
+interface TwinProps {
+  as?: string | Element
+  css?: CSSProp
+  tw?: string
+}
 
 declare module 'twin.macro' {
   // The styled and css imports
@@ -10,24 +17,24 @@ declare module 'twin.macro' {
 }
 
 declare module 'react' {
+  // The forwardRef function
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  function forwardRef<T, P = {}>(
+    render: ForwardRefRenderFunction<T, P & TwinProps>,
+  ): ForwardRefExoticComponent<
+    PropsWithoutRef<P & TwinProps> & RefAttributes<T>
+  >
+
   // The css prop
-  interface HTMLAttributes<T> extends DOMAttributes<T> {
-    css?: CSSProp
-    tw?: string
-  }
+  interface HTMLAttributes<T> extends DOMAttributes<T>, TwinProps {}
   // The inline svg css prop
-  interface SVGProps<T> extends SVGProps<SVGSVGElement> {
-    css?: CSSProp
-    tw?: string
-  }
+  interface SVGProps<T> extends SVGProps<SVGSVGElement>, TwinProps {}
 }
 
 // The 'as' prop on styled components
 declare global {
   namespace JSX {
-    interface IntrinsicAttributes<T> extends DOMAttributes<T> {
-      as?: string | Element
-    }
+    interface IntrinsicAttributes<T> extends DOMAttributes<T>, TwinProps {}
   }
   type StyledComponentType<T = ElementType, P = Record> = ComponentType<
     ComponentPropsWitRef<ElementType> & P
