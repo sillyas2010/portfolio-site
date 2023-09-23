3685ef342c5c2775f0d1f98bb1bbbe2135344a63
sillyas2010 <sillyas2010@gmail.com>
2023-09-23 22:08:29 +0300

    change me block markup

--- src/app/components/Me/styled.tsx
+++ src/app/components/Me/styled.tsx
@@ -9,20 +9,20 @@ export const Content = styled.section`
 `
 
 export const InfoContainer = styled.article`
-  ${tw`grid grid-cols-4 md:grid-rows-1 grid-rows-2 gap-3 mx-auto mt-24 max-w-screen-md`}
+  ${tw`grid grid-cols-4 sm:grid-cols-3 md:grid-rows-1 grid-rows-none gap-3 mx-auto mt-24 max-w-screen-md`}
 `
 
 export const DescriptionContainer = styled.div`
-  ${tw`md:col-span-3 md:row-start-1 col-span-4 col-start-1 row-start-2`}
+  ${tw`col-span-4 col-start-1 row-start-2 md:col-span-2 md:row-start-1 `}
 `
 
 export const AvatarContainer = styled.div`
-  ${tw`md:text-right md:col-start-4 col-start-1 col-span-4 row-start-1`}
+  ${tw`col-start-1 col-span-2 row-start-1 md:text-right md:col-start-3`}
 `
 
 export const AvatarWrapper = styled.picture`
   ${tw`after:content-empty after:absolute after:top-0 after:w-full after:h-full after:overflow-hidden after:animate-avatar after:shadow-avatar after:opacity-50`}
-  ${tw`inline-flex h-full aspect-[1] relative animate-avatar overflow-hidden`}
+  ${tw`inline-flex max-w-full w-56 aspect-[1] relative animate-avatar overflow-hidden`}
 `
 
 export const Avatar = tw`w-full`
@@ -33,7 +33,7 @@ export const Name = styled.h3`
 
 export const SubTitle = styled.div`
   ${tw`relative pl-3 sm:pl-20 mb-2 text-xl`}
-  ${tw`before:content-empty before:absolute before:left-0 before:top-1/2 before:w-2 sm:before:w-16 before:h-px before:bg-bgLight`}
+  ${tw`before:content-empty before:absolute before:left-0 before:top-1/2 before:w-2 sm:before:w-16 before:h-px dark:before:bg-bgLight before:bg-bgDark`}
 `
 
 export const Description = styled.h5`
