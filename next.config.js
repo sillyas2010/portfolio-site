a5961c4043c856b66367eecfbea55e0375879f02
sillyas2010 <sillyas2010@gmail.com>
2023-09-26 00:46:49 +0300

    add svgr

--- next.config.js
+++ next.config.js
@@ -7,6 +7,29 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find(rule =>
+      rule.test?.test?.('.svg'),
+    )
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: { not: /url/ },
+        use: ['@svgr/webpack'],
+      },
+    )
+
+    fileLoaderRule.exclude = /\.svg$/i
+
+    return config
+  },
 }
 
 module.exports = withTwin(nextConfig)
