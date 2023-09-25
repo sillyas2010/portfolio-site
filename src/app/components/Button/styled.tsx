0f6fd70bcb15077965dcd2d5e97100e06d24d600
sillyas2010 <sillyas2010@gmail.com>
2023-09-25 16:43:30 +0300

    add raw Button

--- src/app/components/Button/styled.tsx
+++ src/app/components/Button/styled.tsx
@@ -6,12 +6,14 @@ export const variants = {
   primary: 'PRIMARY',
   secondary: 'SECONDARY',
   outline: 'OUTLINE',
+  raw: 'RAW',
 } as const
 
 export type OneOfButtonVariants = OneOfValues<typeof variants>
 
 const variantStyles = {
   [variants.primary]: tw`bg-orange-500 border-transparent text-white hover:enabled:bg-orange-600 focus:enabled:opacity-90 focus:enabled:ring-orange-600`,
+  [variants.raw]: tw`border-transparent text-orange-500 hover:enabled:bg-neutral-800/10 hover:dark:enabled:bg-neutral-800/50 focus:enabled:bg-neutral-800/10 focus:dark:enabled:bg-neutral-800/50 focus:enabled:outline-none`,
   [variants.secondary]: tw`bg-white border-orange-500 text-orange-500 hover:enabled:bg-orange-600 hover:enabled:border-orange-600 hover:enabled:text-white focus:enabled:border-transparent focus:enabled:ring-orange-600`,
   [variants.outline]: tw`bg-transparent border-orange-400 text-orange-500 hover:enabled:bg-orange-600 hover:enabled:text-white hover:enabled:border-transparent dark:focus:enabled:text-white focus:enabled:border-transparent focus:enabled:ring-orange-600`,
 }
