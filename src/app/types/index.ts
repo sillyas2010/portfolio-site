29d9d07603682e6394c0529f26e616f33db94a14
sillyas2010 <sillyas2010@gmail.com>
2023-10-01 22:24:28 +0300

    transfer OneOfValues type into common types

--- src/app/types/index.ts
+++ src/app/types/index.ts
@@ -1,7 +1,9 @@
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core'
 import { ReactNode } from 'react'
 
-export type NavItem = {
+export type OneOfValues<T extends object> = T[keyof T]
+
+export interface NavItem {
   title: string
   link: string
   icon?: ReactNode
