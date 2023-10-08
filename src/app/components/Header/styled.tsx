83c6e7c5a78828581174eb42620618a17ff68a56
sillyas2010 <sillyas2010@gmail.com>
2023-10-08 13:23:58 +0300

    fix blurred header on safari

--- src/app/components/Header/styled.tsx
+++ src/app/components/Header/styled.tsx
@@ -1,9 +1,5 @@
 import tw, { styled } from 'twin.macro'
 
-export const Header = styled.header`
-  ${tw`w-full h-20 fixed top-0 left-0 z-30 bg-bgDark/10 backdrop-blur-lg flex items-center px-5`}
-`
-
 export const LogoWrapper = styled.div`
   & > svg {
     ${tw`w-48 md:w-40 lg:w-48`}
@@ -17,4 +13,8 @@ export const Content = styled.section`
 export const NavLink = styled.a`
   ${tw`flex items-center dark:text-white text-black`}
   ${tw`hover:opacity-80`}
+`
+
+export const Header = styled.header`
+  ${tw`w-full h-20 fixed top-0 left-0 z-30 bg-bgDark/10 flex items-center px-5 backdrop-blur-lg [-webkit-backdrop-filter:var(--tw-backdrop-blur)]`}
 `
