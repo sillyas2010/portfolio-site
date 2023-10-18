3b286edff5a42e3497069affb1c68663cb52884f
sillyas2010 <sillyas2010@gmail.com>
2023-10-19 01:25:33 +0300

    add isTouched, isAllTouched, isValid and onSubmit for useForm hook

--- src/app/hooks/useForm.ts
+++ src/app/hooks/useForm.ts
@@ -1,83 +1,139 @@
-import { RefObject, useEffect, useRef, useState } from 'react'
+import { RefObject, useCallback, useEffect, useRef, useState } from 'react'
 
 interface useForm<FieldKeys extends string | number | symbol> {
   fields: Record<
     FieldKeys,
     RefObject<HTMLInputElement> | RefObject<HTMLTextAreaElement>
   >
-  onChange?: (field: FieldKeys, value?: string) => void
+  onChange?: (
+    field: FieldKeys,
+    value?: string,
+    fieldElement?: HTMLInputElement | HTMLTextAreaElement | null,
+  ) => void
 }
 
 const useForm = <FieldKeys extends string | number | symbol>({
   fields,
-  onChange,
+  onChange: onFormChange,
 }: useForm<FieldKeys>) => {
+  const possibleFields = Object.keys(fields)
   type ValuesT = Record<keyof typeof fields, string>
   const [values, setValues] = useState<ValuesT>(() =>
-    Object.keys(fields).reduce(
+    possibleFields.reduce(
       (accum, field) => ({ ...accum, [field]: '' }),
       {} as ValuesT,
     ),
   )
   type ErrorsT = Record<keyof typeof fields, boolean | string>
   const errors = useRef<ErrorsT>(
-    Object.keys(fields).reduce(
+    possibleFields.reduce(
       (accum, field) => ({ ...accum, [field]: false }),
       {} as ErrorsT,
     ),
   )
+  const [isValid, setIsValid] = useState(true)
   type TouchedT = Record<keyof typeof fields, boolean>
   const touched = useRef<TouchedT>(
-    Object.keys(fields).reduce(
+    possibleFields.reduce(
       (accum, field) => ({ ...accum, [field]: false }),
       {} as TouchedT,
     ),
   )
+  const [isAllTouched, setIsAllTouched] = useState(false)
+  const isTouched = useRef(false)
+
+  const onSubmit = () => {
+    isTouched.current = true
+    possibleFields.forEach(field => {
+      touched.current[field as keyof typeof fields] = true
+    })
+    setIsAllTouched(true)
+  }
+  const onFieldChange = useCallback(
+    (field: keyof typeof fields) => () => {
+      const fieldElement = fields[field].current
+      const newValue = fieldElement?.value
+      let newError: string | boolean = !fieldElement?.validity?.valid
+
+      if (!fieldElement?.validity?.valid) {
+        newError = fieldElement?.validationMessage || true
+        errors.current[field] = newError
+        setIsValid(false)
+      } else {
+        errors.current[field] = false
+      }
+
+      touched.current[field] = true
+      isTouched.current = true
+
+      setValues(prev => ({
+        ...prev,
+        [field]: newValue?.trim?.() || '',
+      }))
+
+      onFormChange?.(field, newValue, fieldElement)
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [possibleFields.join(), onFormChange],
+  )
 
   useEffect(() => {
-    const events = Object.keys(fields).map(field => {
+    let currentIsValid = true
+    let currentTouched = 0
+
+    for (let i = 0; i < possibleFields.length; i++) {
+      const field = possibleFields[i] as keyof typeof fields
+      errors.current[field] = fields[field]?.current?.validationMessage || false
+
+      if (errors.current[field] !== false) {
+        if (currentIsValid) {
+          currentIsValid = false
+        }
+        if (touched.current[field] === true) {
+          currentTouched += 1
+        }
+      }
+    }
+
+    setIsValid(currentIsValid)
+    if (currentTouched === possibleFields.length) {
+      setIsAllTouched(true)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [possibleFields.join(), values])
+
+  useEffect(() => {
+    const events = possibleFields.map(field => {
       const oneOfFields = field as keyof typeof fields
 
-      return [
-        oneOfFields,
-        () => {
-          const fieldElement = fields[oneOfFields].current
-          const newValue = fieldElement?.value
-          let newError: string | boolean = !fieldElement?.validity?.valid
-
-          if (!fieldElement?.validity?.valid) {
-            newError = fieldElement?.validationMessage || true
-          }
-
-          errors.current[oneOfFields] = newError
-          touched.current[oneOfFields] = true
-
-          setValues(prev => ({
-            ...prev,
-            [oneOfFields]: newValue,
-          }))
-
-          onChange?.(oneOfFields, newValue)
-        },
-      ] as const
+      return [oneOfFields, onFieldChange(oneOfFields)] as const
     })
 
-    events.forEach(([field, onChange]) => {
-      if (onChange) {
-        fields[field]?.current?.addEventListener('change', onChange)
+    events.forEach(([field, onCurrentChange]) => {
+      if (onCurrentChange) {
+        fields[field]?.current?.addEventListener('change', onCurrentChange)
       }
     })
 
     return () => {
-      events.forEach(([field, onChange]) => {
-        if (onChange) {
-          fields[field]?.current?.removeEventListener('change', onChange)
+      events.forEach(([field, onCurrentChange]) => {
+        if (onCurrentChange) {
+          fields[field]?.current?.removeEventListener('change', onCurrentChange)
         }
       })
     }
-  }, [fields, onChange])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [possibleFields.join(), onFormChange])
 
-  return { values, errors: errors.current, touched: touched.current }
+  return {
+    values,
+    errors: errors.current,
+    touched: touched.current,
+    isTouched: isTouched.current,
+    isAllTouched,
+    isValid,
+    onSubmit,
+  }
 }
 
 export default useForm
