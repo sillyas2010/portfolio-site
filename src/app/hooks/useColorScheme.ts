879a8b26bd156612e3acb4e3a661826ac856ad6e
sillyas2010 <sillyas2010@gmail.com>
2023-10-20 17:26:58 +0300

    add colorScheme subscription

--- src/app/hooks/useColorScheme.ts
+++ src/app/hooks/useColorScheme.ts
@@ -1,10 +1,14 @@
 import cookies from '@boiseitguru/cookie-cutter'
-import { useLayoutEffect, useRef, useState } from 'react'
+import { useEffect, useLayoutEffect, useRef, useState } from 'react'
 
 import { colorScheme, cookieName } from '@/app/constants/colorScheme'
 
-export default function useColorScheme(isInitialDark = true) {
+export default function useColorScheme(
+  isSubscribed = false,
+  isInitialDark = true,
+) {
   const [isDark, setIsDark] = useState<boolean>(isInitialDark)
+  const colorSchemeObserver = useRef<MutationObserver | null>(null)
   const isInitialRender = useRef(true)
 
   useLayoutEffect(() => {
@@ -27,6 +31,37 @@ export default function useColorScheme(isInitialDark = true) {
       }
     }
   }, [isDark])
+
+  useEffect(() => {
+    if (colorSchemeObserver.current) {
+      colorSchemeObserver.current.disconnect()
+    }
+
+    colorSchemeObserver.current = new MutationObserver(mutations => {
+      mutations.forEach(mutation => {
+        if (mutation.attributeName === 'class') {
+          const isDark = document.documentElement.classList.contains(
+            colorScheme.dark,
+          )
+
+          setIsDark(isDark)
+        }
+      })
+    })
+
+    if (isSubscribed) {
+      colorSchemeObserver.current?.observe(document.documentElement, {
+        attributes: true,
+        attributeFilter: ['class'],
+      })
+    } else {
+      colorSchemeObserver.current?.disconnect()
+    }
+
+    return () => {
+      colorSchemeObserver.current?.disconnect()
+    }
+  }, [isSubscribed])
 
   return {
     isDark,
